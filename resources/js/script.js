960b4fb4b382c107f4a4026b0e6b7c78eefc3ab1
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2023-12-06 01:30:58 +0500

    1.2

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,8 +2,19 @@ const myLeads = [];
 
 const inputEl = document.querySelector("#input-el")
 const saveBtnEl = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 saveBtnEl.addEventListener('click',function(){
-    myLeads.push("www.awesomelead.com");
-    console.log(myLeads);
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
 });
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a href="${myLeads[i]}" target="_blank"> ${myLeads[i]} </a></li>`;
+    }
+    console.log(listItems)
+    ulEl.innerHTML = listItems;
+}
