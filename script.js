fd45e8540632956f38cbe97ed550ebbef8f97072
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2023-12-07 02:13:30 +0500

    1.3

--- script.js
+++ script.js
@@ -15,6 +15,5 @@ function renderLeads(){
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `<li><a href="${myLeads[i]}" target="_blank"> ${myLeads[i]} </a></li>`;
     }
-    console.log(listItems)
     ulEl.innerHTML = listItems;
 }
