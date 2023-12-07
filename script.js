49823d8e0c5a3774067887da5f27d3ca2653a8eb
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2023-12-08 00:29:34 +0500

    1.5

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 let myLeads = []
 
+
 const inputEl = document.querySelector("#input-el")
 const saveBtnEl = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
 saveBtnEl.addEventListener('click',function(){
-    myLeads = JSON.parse(myLeads);
     myLeads.push(inputEl.value);
     inputEl.value = "";
     renderLeads();
-    // Save the myLeads array to localStorage 
-    myLeads = JSON.stringify(myLeads)
-    localStorage.setItem("Leads",myLeads);
-
-
-    console.log(localStorage.getItem(myLeads));
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads(){
