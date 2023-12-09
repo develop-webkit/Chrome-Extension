3d6956d17a008cd56d2906a6b8a1d87a7e267eba
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2023-12-09 21:27:41 +0500

    1.6

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-let myLeads = []
-
-
+let myLeads = [];
 const inputEl = document.querySelector("#input-el")
 const saveBtnEl = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
+const delBtn = document.querySelector("#delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBTN = document.querySelector("#tab-btn");
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    renderli(myLeads);
+}
+
+function renderli(leads){
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `<li><a href="${leads[i]}" target="_blank"> ${leads[i]} </a></li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBTN.addEventListener('click',function(){
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    renderli(myLeads);
+})
+
+
+delBtn.addEventListener('dblclick',function(){
+    myLeads = [];
+    localStorage.clear();
+    renderli(myLeads);
+});
 
 saveBtnEl.addEventListener('click',function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    renderLeads();
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    console.log(localStorage.getItem("myLeads"));
+    renderli(myLeads);
 });
 
-function renderLeads(){
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a href="${myLeads[i]}" target="_blank"> ${myLeads[i]} </a></li>`;
-    }
-    ulEl.innerHTML = listItems;
-}
