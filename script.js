0a3296b1474f93419d87b480e2c730ebaa39bc8e
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2023-12-12 05:36:43 +0500

    1.7

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
 let myLeads = [];
-const inputEl = document.querySelector("#input-el")
+let myTitle = [];
+const inputEl = document.querySelector("#input-el");
+const titleEl = document.querySelector("#title-el");
 const saveBtnEl = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const delBtn = document.querySelector("#delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const titleFromlocalStorage = JSON.parse(localStorage.getItem("myTitle"));
 const tabBTN = document.querySelector("#tab-btn");
 
-if(leadsFromLocalStorage){
+if(leadsFromLocalStorage && titleFromlocalStorage){
     myLeads = leadsFromLocalStorage;
-    renderli(myLeads);
+    myTitle = titleFromlocalStorage;
+    renderli(myLeads,myTitle);
 }
 
-function renderli(leads){
-    let listItems = ""
+function renderli(leads,title){
+    let listItems = "";
     for (let i = 0; i < leads.length; i++) {
-        listItems += `<li><a href="${leads[i]}" target="_blank"> ${leads[i]} </a></li>`;
+        listItems += `<li><a href="${leads[i]}" target="_blank"> ${title[i]} </a></li>`;
     }
     ulEl.innerHTML = listItems;
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 tabBTN.addEventListener('click',function(){
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderli(myLeads);
-})
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myTitle.push(tabs[0].title);
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        localStorage.setItem("myTitle",JSON.stringify(myTitle));
+        renderli(myLeads,myTitle);
+    });
+});
 
 
 delBtn.addEventListener('dblclick',function(){
-    myLeads = [];
-    localStorage.clear();
-    renderli(myLeads);
+        myLeads = [];
+        myTitle = [];
+        localStorage.clear();
+        renderli(myLeads,myTitle);
 });
 
 saveBtnEl.addEventListener('click',function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderli(myLeads);
+    if(inputEl.value && titleEl.value){
+        myLeads.push(inputEl.value);
+        myTitle.push(titleEl.value);
+        inputEl.value = "";
+        titleEl.value = "";
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        localStorage.setItem("myTitle",JSON.stringify(myTitle));
+        renderli(myLeads,myTitle);
+    }
 });
 
